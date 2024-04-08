ca9e3e8acab6bea52c64e607f07c5a428fbd7c50
pl9shy <neo.wwh1@gmail.com>
2024-04-08 18:53:25 +0500

    edited. interface updated

--- reg_project.py
+++ reg_project.py
@@ -1,9 +1,15 @@
 import re
 import os
 import glob
+import json
+import time
+
+print('Анализ файлов Python.\n')
+time.sleep(3)
+print('Идёт подготовка файлов, подождите.. \n')
 
 
-class Analyzer():  #Реализует функции в виде методов класса.
+class Analyzer():  # Реализует функции в виде методов класса.
     def __init__(self):
         self.info = {}
         self.__py_files = self.__search_py_files()
@@ -24,21 +30,30 @@ class Analyzer():  #Реализует функции в виде методов
 
     def save_info(self, filename):
         """Сохраняет данные о файле."""
-        with open(filename, 'w') as python_file:
+        with open(filename, 'w', encoding='utf-8', errors='ignore') as info_file:
             for python_file in self.__py_files:
-                with open(python_file, 'r'):
-                    file_code = python_file.read()
-                    info_result = self.get_file_info(file_code)
-                    self.info[python_file] = info_result
+                with open(python_file, 'r', encoding='utf-8') as pf:
+                    try:
+                        file_code = pf.read()
+                        info_result = self.get_file_info(file_code)
+                        self.info[python_file] = info_result
+                    except Exception as e:
+                        print(f'Ошибка при обработке файла: {python_file}')
+            json.dump(self.info, info_file)
 
-    def insert_data(self):
+    def show_data(self, filename):
         """Сохраняет информацию о программах в текстовый файл."""
-        with open('analyze_result.txt', 'w', encoding='utf-8') as result_file:
+        with open(filename, 'w', encoding='utf-8', errors='ignore') as result_file:
             for key, value in self.info.items():
                 result_file.write(f'Программа: {key}\n')
-                result_file.write(f'Результат анализа: {value}')
+                result_file.write(f'Результат анализа: {value}\n\n')
 
 
 analyzer_beta = Analyzer()
-analyzer_beta.insert_data()
-print(analyzer_beta.info)
+
+result_beta_file_name = input('Введите название первого файла: ')
+result_file_name = input('Введите название второго файла: ')
+print('\nИдёт анализ..\n')
+analyzer_beta.save_info(f'{result_beta_file_name}.txt')
+analyzer_beta.show_data(f'{result_file_name}.txt')
+print('\nАнализ завершён!')
